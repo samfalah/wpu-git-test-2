e0ce9b42953a4b20e7690be590362348e082b5ad
samfalah <sam.falah.95@gmail.com>
2023-03-31 09:11:34 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h3>Hello World !</h3>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus minus ea explicabo, dolores ducimus dicta eum, illum non esse nam id voluptas inventore. Iste, veritatis voluptatibus repellat provident labore mollitia?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
